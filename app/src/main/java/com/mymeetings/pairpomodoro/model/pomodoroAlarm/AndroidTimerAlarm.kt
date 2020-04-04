e1b850ca4685db34a70a30558fb916f4cbdd2be3
Prasanna Anbazhagan <praslnx8@gmail.com>
2020-04-04 20:38:06 +0530

    Added alarm type sound.

--- app/src/main/java/com/mymeetings/pairpomodoro/model/pomodoroAlarm/AndroidTimerAlarm.kt
+++ app/src/main/java/com/mymeetings/pairpomodoro/model/pomodoroAlarm/AndroidTimerAlarm.kt
@@ -2,6 +2,8 @@ package com.mymeetings.pairpomodoro.model.pomodoroAlarm
 
 import android.content.Context
 import android.content.res.AssetFileDescriptor
+import android.media.AudioAttributes
+import android.media.AudioManager
 import android.media.MediaPlayer
 
 class AndroidTimerAlarm(private val context: Context) : TimerAlarm {
@@ -11,6 +13,12 @@ class AndroidTimerAlarm(private val context: Context) : TimerAlarm {
     override fun alarm(timerAlarmType: TimerAlarmType) {
         val afd: AssetFileDescriptor = context.assets.openFd(audioFileName)
         val player = MediaPlayer()
+        player.setAudioAttributes(AudioAttributes.Builder()
+            .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+            .setLegacyStreamType(AudioManager.STREAM_ALARM)
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build())
         player.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
         player.prepare()
         player.start()
